0530a15aebbd6c5164a7a229c728437e4fa2c079
Justin <jbbenson12@gmail.com>
2019-08-31 17:20:48 -1000

    auto

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -1,12 +1,23 @@
 -- db indexed_cars
+DROP TABLE IF EXISTS car_models;
 
-CREATE TABLE IF NOT EXISTS car_models
+CREATE TABLE
+IF NOT EXISTS car_models
 (
  id serial,
- make_code character varying(125) NOT NULL,
- make_title character varying(125) NOT NULL,
- model_code character varying(125) NOT NULL,
- model_title character varying(125) NOT NULL,
+ make_code character varying
+(125) NOT NULL,
+ make_title character varying
+(125) NOT NULL,
+ model_code character varying
+(125) NOT NULL,
+ model_title character varying
+(125) NOT NULL,
  year integer NOT NULL,
- PRIMARY KEY (id)
-)
+ PRIMARY KEY
+(id)
+);
+
+CREATE INDEX idx_car_make ON car_models (make_code);
+CREATE INDEX idx_car_model ON car_models (model_code);
+CREATE INDEX idx_car_year ON car_models (year);
